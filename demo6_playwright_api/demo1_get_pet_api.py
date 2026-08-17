1dc35d607fe3c6ce0746200b1e50e27977c73f52
balaji-githubstore <balajidinakaran1@gmail.com>
2026-08-17 13:16:56 +0530

    findbypet and status api details

---
+++ demo6_playwright_api/demo1_get_pet_api.py
@@ -0,0 +1,36 @@
+from playwright.sync_api import sync_playwright
+
+# test_search_valid_pet_id
+# test_search_invalid_pet_id
+
+
+with sync_playwright() as p:
+    api_context = p.request.new_context(
+        base_url="https://petstore.swagger.io/v2/")
+
+    response = api_context.get("pet/5")
+
+    print(response.status)
+    print(response.status_text)
+    print(response.json())
+    print(response.text())
+    print(response.headers)
+
+    print(response.headers)
+    print(response.headers["date"])
+
+    # kind of dic right now
+    response_body = response.json()
+
+    print(response_body["id"])
+    print(response_body["name"])
+    print(response_body["status"])
+    print(response_body["category"])
+    print(response_body["category"]["id"])
+    print(response_body["category"]["name"])
+
+    # print id and name from tags
+    print(response_body["tags"])
+    print(response_body["tags"][0])
+    print(response_body["tags"][0]['id'])
+    print(response_body["tags"][0]['name'])
---
+++ demo6_playwright_api/demo2_find_status.py
@@ -0,0 +1,15 @@
+from playwright.sync_api import sync_playwright
+
+# test_search_valid_status
+# test_search_invalid_status
+
+
+with sync_playwright() as p:
+    api_context = p.request.new_context(
+        base_url="https://petstore.swagger.io/v2/")
+
+    response = api_context.get("pet/findByStatus?status=sold")
+
+    print(response.status)
+    # print(response.json())
+    print(response.json()[2]['status'])
